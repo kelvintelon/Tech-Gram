d022ab1a78bd9fe58170eb153f21c06c7042e94c
Nayef Taleb <nayeftaleb@gmail.com>
2022-04-12 16:18:45 -0400

    update

---
+++ vue/src/views/Deletepage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      
+    <div class="sideMenu"><side-menu></side-menu></div>
+    <header-logo></header-logo>
+
+    
+  </div>
+</template>
+
+<script>
+import HeaderLogo from "../components/HeaderLogo.vue";
+import SideMenu from "../components/SideMenu.vue";
+
+export default {
+components: {
+    HeaderLogo,
+    SideMenu,
+  },
+}
+</script>
+
+<style>
+
+</style>
