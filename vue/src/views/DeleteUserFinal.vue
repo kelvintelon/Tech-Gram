f9ae8303668cf9222f14695a126d14edf8fafbd2
Oxana Howery <oxana.howery@gmail.com>
2022-04-13 09:37:42 -0400

    delete button

---
+++ vue/src/views/DeleteUserFinal.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="finalForm">
+    <h1>Tech Gram</h1>
+    <form class="toDelete" @submit.prevent="deleteUser">
+      <div class="alert" role="alert" v-if="invalidCredentials">
+        Invalid username or password!
+      </div>
+      <label for="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        class="form-control"
+        placeholder="Username"
+        v-model="user.username"
+        required
+      />
+      <label for="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        class="form-control"
+        placeholder="Password"
+        v-model="user.password"
+        required
+      />
+      <button class="deleteButtonFinal" type="submit">Delete</button>
+    </form>
+    <button class="deleteButtonFinal" @click="$router.push('/')">
+      Go Back
+    </button>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Fredoka+One"
+    />
+  </div>
+</template>
+
+<script>
+import authService from "../services/AuthService";
+
+export default {
+  name: "deleteUserFinal",
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+      invalidCredentials: false,
+    };
+  },
+  methods: {
+  deleteUser() {
+    authService
+        .login(this.user)
+        .then((response) => {
+          if (response.status == 200) {
+          authService.delete(this.user)
+          this.$router.push('/logout');
+        }
+      })
+      .catch((error) => {
+        const response = error.response;
+
+        if (response.status === 401) {
+          this.invalidCredentials = true;
+        }
+      });
+  },
+  }
+};
+</script>
+
+<style>
+.finalForm {
+  border: 2px solid black;
+  background-color: aliceblue;
+  position: absolute;
+  margin: auto;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 500px;
+  height: 600px;
+  text-align: center;
+}
+.deleteButtonFinal {
+  background-color: #00adee;
+  border: 1px solid black;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  color: black;
+  padding: 15px 32px;
+  font-weight: bold;
+  font-size: 16px;
+  cursor: pointer;
+  margin-top: 30px;
+  margin-bottom: 110px;
+}
+.deleteButton:hover {
+  background-color: #167fa5;
+  transition: 0.7s;
+}
+h1 {
+  margin-top: 60px;
+  margin-bottom: 40px;
+  font-size: 40px;
+  font-family: Fredoka One;
+}
+</style>
