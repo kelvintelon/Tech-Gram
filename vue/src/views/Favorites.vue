93897409e14b7781fcea627347676b9f61eb89a1
kelvintelon <telon.kelvin77@gmail.com>
2022-04-14 09:17:47 -0400

    what I have so far

--- vue/src/views/Favorites.vue
+++ vue/src/views/Favorites.vue
@@ -3,6 +3,8 @@
     <div class="sideMenu"><side-menu></side-menu></div>
     <header-logo></header-logo>
     <picture-card></picture-card>
+  <div>{{ photo_id }}</div>
+  <div>{{ user_id }}</div>
   </div>
 </template>
 
@@ -11,7 +13,27 @@ import HeaderLogo from "../components/HeaderLogo.vue";
 import SideMenu from "../components/SideMenu.vue";
 import PictureCard from "../components/PictureCard.vue";
 
+import favoriteService from "../services/AuthService"
+
 export default {
+  data() {
+    return {
+        favoritePhoto: {
+          favorite_id: 0,
+          photo_id: 0,
+          user_id: 0,
+          caption: "",
+          image_location: "",
+          date_and_time: ""
+        },
+    }
+  },
+  mounted() {
+      favoriteService.getFavorites().then(response => {
+        this.photo_id = response.data.photo_id;
+        this.user_id = response.data.user_id;
+      })
+  },
   components: {
     HeaderLogo,
     SideMenu,
