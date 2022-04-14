10991de454c9edc527288f04918a20f701c1380d
Oxana Howery <oxana.howery@gmail.com>
2022-04-14 09:43:31 -0400

    changes

--- vue/src/services/UploadFileService.js
+++ vue/src/services/UploadFileService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // change the http address later
 const http = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:8080",
     headers:{
         "Content-type":"application/json"
     }
